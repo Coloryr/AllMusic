8585e67d9e77ad02a2cb0cad6d4e101d901dfcfc
Color_yr <402067010@qq.com>
2026-07-18 17:51:58 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ allprojects {
     apply(plugin = "com.gradleup.shadow")
 
     group = "com.coloryr"
-    version = "4.1.2"
+    version = "4.1.3"
 
     // 设置项目JDK版本
     java.sourceCompatibility = JavaVersion.VERSION_1_8
